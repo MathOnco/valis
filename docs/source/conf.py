1ac7cde853675a25fcc9d3ec9de525743ab553e2
cdgatenbee <chandlergatenbee@gmail.com>
2022-01-04 15:53:56 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'VALIS'
+copyright = '2022, Gatenbee'
+author = 'Chandler Gatenbee'
 
 release = '0.1'
 version = '0.1.0'
