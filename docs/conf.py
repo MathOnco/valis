b49916a0842805572b49c7d53cf239e817a4335b
Chandler Gatenbee <chandlergatenbee@gmail.com>
2023-01-31 12:30:19 -0500

    poetry ci

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "./_images/valis_logo_black_no_bg.png"
+html_logo = "https://github.com/MathOnco/valis/raw/main/docs/_images/valis_logo_black_no_bg.png"
 html_theme_options = {
     # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
     # 'analytics_anonymize_ip': False,
